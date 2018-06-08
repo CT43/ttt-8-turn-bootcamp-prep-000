c579ebcd934f35ce6b83078abc73b548e061a778
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-06-08 00:06:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,18 @@ def move(board, position, x_or_o = "X")
   
 end
 
+def valid_move? (board, index)
+  if !position_taken?(board, index) == true && index.between?(0, 8)
+      return true
+  else
+    return false
+  end
+end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
